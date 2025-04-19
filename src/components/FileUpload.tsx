fcceb351fe27110c373dc295af820191d6943e0e
HUy2221 <HUy2221@users.noreply.github.com>
2025-04-20 01:00:55 +0700

    test 4232

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -172,17 +172,6 @@ const FileUpload: React.FC = () => {
                   <line x1="16" y1="13" x2="8" y2="13"></line>
             </div>
           )}
-        </div>
-            {uploadStatus === "uploading" ? (
-              <div className="spinner"></div>
-            ) : uploadStatus === "success" ? (
-              <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
                   stroke="currentColor"
                   strokeWidth="2"
                   strokeLinecap="round"
