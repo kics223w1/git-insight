c17644bb876d1d539e2d09e2420ef61b384aa4ec
HUy2221 <HUy2221@users.noreply.github.com>
2025-04-20 11:45:20 +0700

    test commit and push

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -167,10 +167,6 @@ const FileUpload: React.FC = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 >
-                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                  <polyline points="14 2 14 8 20 8"></polyline>
-                  <line x1="16" y1="13" x2="8" y2="13"></line>
-            </div>
           )}
                   <polyline points="20 6 9 17 4 12"></polyline>
                 </svg>
